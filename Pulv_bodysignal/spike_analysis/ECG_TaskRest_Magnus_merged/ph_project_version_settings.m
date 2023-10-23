5e83e51ac8ea78ea5894be582a109c93c7376e99
luschn <lukas.schneider.m@gmail.com>
2023-10-23 14:14:24 +0200

    Update ph_project_version_settings.m

--- Pulv_bodysignal/spike_analysis/ECG_TaskRest_Magnus_merged/ph_project_version_settings.m
+++ Pulv_bodysignal/spike_analysis/ECG_TaskRest_Magnus_merged/ph_project_version_settings.m
@@ -7,21 +7,18 @@ keys.cal.datasets                   = [4, 8]; %% should probably be renamed to b
 keys.batching.targets               ={'dPul','VPL', 'MD', 'AIP', 'PCC', 'PPC'};
 
 
-% %% Localization (chamberlain keys)
-% cc=0;
-% 
-% % Linus right
-% cc=cc+1;
-% keys.loc(cc).tt.choice = 0;
-% keys.loc(cc).tt.tasktypes = {'Msac_opt'};
-% keys.loc(cc).vmr_path = 'Y:\Projects\Pulv_microstim_behavior\behavior\electrode_localization\Linus\LI_20130614_STEREO_neurological-TO-LI_20150619_chamR_normal_128.vmr';
-% keys.loc(cc).z_offset_mm = 26; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
-% keys.loc(cc).monkey_prefix = 'LI_';
-% keys.loc(cc).monkey='Lin';
-% keys.loc(cc).target_area='dPulv_r';
-% keys.loc(cc).grid_id = 'GRID.22.2';
-% keys.loc(cc).significance_to_plot = 'visuomotor';
-% keys.loc(cc).saggital_or_coronal = 'coronal';
+%% Localization (chamberlain keys)
+cc=0;
 
-% %% population PSTH settings
-% 1
+% Linus right
+cc=cc+1;
+keys.loc(cc).tt.choice = 0;
+keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
+keys.loc(cc).vmr_path = 'Y:\Projects\Pulv_microstim_behavior\behavior\electrode_localization\Linus\LI_20130614_STEREO_neurological-TO-LI_20150619_chamR_normal_128.vmr';
+keys.loc(cc).z_offset_mm = 26; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
+keys.loc(cc).monkey_prefix = 'MA_';
+keys.loc(cc).monkey='Mag';
+keys.loc(cc).target_area='MD_R';
+keys.loc(cc).grid_id = 'GRID.22.2';
+keys.loc(cc).significance_to_plot = 'ungrouped';
+keys.loc(cc).saggital_or_coronal = 'coronal';
