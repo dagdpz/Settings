b67be7eb1aabc9f328644df3d52d55d5b47cd390
lubavas <19555010+lubavas@users.noreply.github.com>
2024-04-23 13:05:24 +0200

    Update ph_project_version_settings.m

--- Pulv_bodysignal/spike_analysis/ECG_TaskRest_Bacchus_merged/ph_project_version_settings.m
+++ Pulv_bodysignal/spike_analysis/ECG_TaskRest_Bacchus_merged/ph_project_version_settings.m
@@ -8,21 +8,69 @@ keys.batching.targets               ={'dPul','VPL', 'MD'};
 keys.tt.tasktypes                   ={'Fsac_opt','Vsac_opt'};
 
 % %% Localization (chamberlain keys)
-% cc=0;
-% 
-% % Linus right
+cc=0;
+
+% % Bacchus right VPL
 % cc=cc+1;
-% keys.loc(cc).tt.choice = 0;
-% keys.loc(cc).tt.tasktypes = {'Msac_opt'};
-% keys.loc(cc).vmr_path = 'Y:\Projects\Pulv_microstim_behavior\behavior\electrode_localization\Linus\LI_20130614_STEREO_neurological-TO-LI_20150619_chamR_normal_128.vmr';
-% keys.loc(cc).z_offset_mm = 26; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
-% keys.loc(cc).monkey_prefix = 'LI_';
-% keys.loc(cc).monkey='Lin';
-% keys.loc(cc).target_area='dPulv_r';
+% keys.loc(cc).tt.choice = NaN;
+% keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
+% keys.loc(cc).vmr_path = 'Y:\MRI\Bacchus\20191115_anatomy\dicom\0103\BA_20191115_chamber_normal_R_neurological_0.25mm.vmr';
+% keys.loc(cc).z_offset_mm = 23.25; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
+% keys.loc(cc).monkey_prefix = 'BA_';
+% keys.loc(cc).monkey='Bac';
+% keys.loc(cc).target_area='VPL_R';
 % keys.loc(cc).grid_id = 'GRID.22.2';
-% keys.loc(cc).significance_to_plot = 'visuomotor';
+% keys.loc(cc).significance_to_plot = 'ungrouped';
 % keys.loc(cc).saggital_or_coronal = 'coronal';
 
-% %% population PSTH settings
-% 1
+% % % Bacchus right pulvinar
+% cc=cc+1;
+% keys.loc(cc).tt.choice = NaN;
+% keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
+% keys.loc(cc).vmr_path = 'Y:\MRI\Bacchus\20191115_anatomy\dicom\0103\BA_20191115_chamber_normal_R_neurological_0.25mm.vmr';
+% keys.loc(cc).z_offset_mm = 23.25+7; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
+% keys.loc(cc).monkey_prefix = 'BA_';
+% keys.loc(cc).monkey='Bac';
+% keys.loc(cc).target_area='dPul_R';
+% keys.loc(cc).grid_id = 'GRID.22.2';
+% keys.loc(cc).significance_to_plot = 'ungrouped';
+% keys.loc(cc).saggital_or_coronal = 'coronal';
 
+% % Bacchus left pulvinar
+% cc=cc+1;
+% keys.loc(cc).tt.choice = NaN;
+% keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
+% keys.loc(cc).vmr_path = 'Y:\MRI\Bacchus\20191115_anatomy\dicom\0104\BA_20191115_chamber_normal_L_neurological_0.25mm.vmr';
+% keys.loc(cc).z_offset_mm = 19.25; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
+% keys.loc(cc).monkey_prefix = 'BA_';
+% keys.loc(cc).monkey='Bac';
+% keys.loc(cc).target_area='dPul_L';
+% keys.loc(cc).grid_id = 'GRID.22.2';
+% keys.loc(cc).significance_to_plot = 'ungrouped';
+% keys.loc(cc).saggital_or_coronal = 'coronal';
+
+% % Bacchus left MD
+% cc=cc+1;
+% keys.loc(cc).tt.choice = NaN;
+% keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
+% keys.loc(cc).vmr_path = 'Y:\MRI\Bacchus\20191115_anatomy\dicom\0104\BA_20191115_chamber_normal_L_neurological_0.25mm.vmr';
+% keys.loc(cc).z_offset_mm = 19.25; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
+% keys.loc(cc).monkey_prefix = 'BA_';
+% keys.loc(cc).monkey='Bac';
+% keys.loc(cc).target_area='MD_L';
+% keys.loc(cc).grid_id = 'GRID.22.2';
+% keys.loc(cc).significance_to_plot = 'ungrouped';
+% keys.loc(cc).saggital_or_coronal = 'coronal';
+
+% Bacchus right MD
+cc=cc+1;
+keys.loc(cc).tt.choice = NaN;
+keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
+keys.loc(cc).vmr_path = 'Y:\MRI\Bacchus\20191115_anatomy\dicom\0103\BA_20191115_chamber_normal_R_neurological_0.25mm.vmr';
+keys.loc(cc).z_offset_mm = 23.25; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
+keys.loc(cc).monkey_prefix = 'BA_';
+keys.loc(cc).monkey='Bac';
+keys.loc(cc).target_area='MD_R';
+keys.loc(cc).grid_id = 'GRID.22.2';
+keys.loc(cc).significance_to_plot = 'ungrouped';
+keys.loc(cc).saggital_or_coronal = 'coronal';
