da24bba1fba4a946fe4ad2378c3ec6ba2bff95e0
lubavas <19555010+lubavas@users.noreply.github.com>
2024-11-05 11:53:16 +0100

    settings reorganization + by unit selection list

--- Pulv_bodysignal/spike_analysis/ECG_TaskRest_Bacchus_merged/ph_project_version_settings.m
+++ Pulv_bodysignal/spike_analysis/ECG_TaskRest_Bacchus_merged/ph_project_version_settings.m
@@ -7,14 +7,17 @@ keys.cal.datasets                   = [5,6,7];
 keys.batching.targets               ={'dPul','VPL', 'MD'}; % {'dPul','VPL', 'MD'};
 keys.tt.tasktypes                   ={'Fsac_opt','Vsac_opt'};
 
-% %% Localization (chamberlain keys)
+%% Localization (chamberlain keys)
+selection_lists = {'_after_SNR_exclusion_stable_noLow_amplitude_ccs_any', '_after_SNR_exclusion_selected'};
+list_folders    = {'unit_lists_ECG', 'unit_lists'};
+
 cc=0;
 
-% keys.loc(cc).tt.choice = NaN;
-% keys.loc(cc).grid_id = 'GRID.22.2';
-% keys.loc(cc).saggital_or_coronal = 'coronal';
-% 
-% % Bacchus right VPL saggital
+for listNum = 1:length(selection_lists)
+
+    for slicePlane = {'coronal', 'sagittal'}
+    
+% % Bacchus right VPL coronal
 % cc=cc+1;
 % keys.loc(cc).tt.choice = NaN;
 % keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
@@ -26,35 +29,11 @@ cc=0;
 % keys.loc(cc).area_color=[1 0.53 0];
 % keys.loc(cc).grid_id = 'GRID.22.2';
 % keys.loc(cc).significance_to_plot = 'ungrouped';
-% keys.loc(cc).saggital_or_coronal = 'sagittal';
-keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
-keys.loc(cc).vmr_path = 'Y:\MRI\Bacchus\20191115_anatomy\dicom\0103\BA_20191115_chamber_normal_R_neurological_0.25mm.vmr';
-keys.loc(cc).z_offset_mm = 23.25; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
-keys.loc(cc).monkey_prefix = 'BA_';
-keys.loc(cc).monkey='Bac';
-keys.loc(cc).target_area='VPL_R';
-keys.loc(cc).area_color=[1 0.53 0];
-keys.loc(cc).grid_id = 'GRID.22.2';
-keys.loc(cc).significance_to_plot = 'ungrouped';
-keys.loc(cc).saggital_or_coronal = 'coronal';
-keys.loc(cc).unit_list = 'Y:\Projects\Pulv_bodysignal\ECG_triggered_spikes\ECG_Bacchus_TaskRest\unit_lists_ECG\unitInfo_after_SNR_exclusion_stable_noLow_amplitude_ccs_any.mat';
+% keys.loc(cc).saggital_or_coronal = slicePlane{1};
+% keys.loc(cc).unit_list = ['Y:\Projects\Pulv_bodysignal\ECG_triggered_spikes\ECG_Bacchus_TaskRest\' list_folders{listNum} '\unitInfo' selection_lists{listNum} '.mat'];
+% keys.loc(cc).folder_suffix        = selection_lists{listNum};
 
-% Bacchus right VPL saggital
-cc=cc+1;
-keys.loc(cc).tt.choice = NaN;
-keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
-keys.loc(cc).vmr_path = 'Y:\MRI\Bacchus\20191115_anatomy\dicom\0103\BA_20191115_chamber_normal_R_neurological_0.25mm.vmr';
-keys.loc(cc).z_offset_mm = 23.25; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
-keys.loc(cc).monkey_prefix = 'BA_';
-keys.loc(cc).monkey='Bac';
-keys.loc(cc).target_area='VPL_R';
-keys.loc(cc).area_color=[1 0.53 0];
-keys.loc(cc).grid_id = 'GRID.22.2';
-keys.loc(cc).significance_to_plot = 'ungrouped';
-keys.loc(cc).saggital_or_coronal = 'sagittal';
-keys.loc(cc).unit_list = 'Y:\Projects\Pulv_bodysignal\ECG_triggered_spikes\ECG_Bacchus_TaskRest\unit_lists_ECG\unitInfo_after_SNR_exclusion_stable_noLow_amplitude_ccs_any.mat';
-
-% % Bacchus right pulvinar coronal
+% % Bacchus right pulvinar
 cc=cc+1;
 keys.loc(cc).tt.choice = NaN;
 keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
@@ -66,23 +45,9 @@ keys.loc(cc).target_area='dPul_R';
 keys.loc(cc).area_color=[0.05 0.65 0.7];
 keys.loc(cc).grid_id = 'GRID.22.2';
 keys.loc(cc).significance_to_plot = 'ungrouped';
-keys.loc(cc).saggital_or_coronal = 'coronal';
-keys.loc(cc).unit_list = 'Y:\Projects\Pulv_bodysignal\ECG_triggered_spikes\ECG_Bacchus_TaskRest\unit_lists_ECG\unitInfo_after_SNR_exclusion_stable_noLow_amplitude_ccs_any.mat';
-
-% Bacchus right pulvinar sagittal
-cc=cc+1;
-keys.loc(cc).tt.choice = NaN;
-keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
-keys.loc(cc).vmr_path = 'Y:\MRI\Bacchus\20191115_anatomy\dicom\0103\BA_20191115_chamber_normal_R_neurological_0.25mm.vmr';
-keys.loc(cc).z_offset_mm = 23.25; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
-keys.loc(cc).monkey_prefix = 'BA_';
-keys.loc(cc).monkey='Bac';
-keys.loc(cc).target_area='dPul_R';
-keys.loc(cc).area_color=[0.05 0.65 0.7];
-keys.loc(cc).grid_id = 'GRID.22.2';
-keys.loc(cc).significance_to_plot = 'ungrouped';
-keys.loc(cc).saggital_or_coronal = 'sagittal';
-keys.loc(cc).unit_list = 'Y:\Projects\Pulv_bodysignal\ECG_triggered_spikes\ECG_Bacchus_TaskRest\unit_lists_ECG\unitInfo_after_SNR_exclusion_stable_noLow_amplitude_ccs_any.mat';
+keys.loc(cc).saggital_or_coronal = slicePlane{1};
+keys.loc(cc).unit_list = ['Y:\Projects\Pulv_bodysignal\ECG_triggered_spikes\ECG_Bacchus_TaskRest\' list_folders{listNum} '\unitInfo' selection_lists{listNum} '.mat'];
+keys.loc(cc).folder_suffix        = selection_lists{listNum};
 
 % Bacchus left pulvinar
 cc=cc+1;
@@ -96,80 +61,40 @@ keys.loc(cc).target_area='dPul_L';
 keys.loc(cc).area_color=[0.05 0.65 0.7];
 keys.loc(cc).grid_id = 'GRID.22.2';
 keys.loc(cc).significance_to_plot = 'ungrouped';
-keys.loc(cc).saggital_or_coronal = 'coronal';
-keys.loc(cc).unit_list = 'Y:\Projects\Pulv_bodysignal\ECG_triggered_spikes\ECG_Bacchus_TaskRest\unit_lists_ECG\unitInfo_after_SNR_exclusion_stable_noLow_amplitude_ccs_any.mat';
+keys.loc(cc).saggital_or_coronal = slicePlane{1};
+keys.loc(cc).unit_list = ['Y:\Projects\Pulv_bodysignal\ECG_triggered_spikes\ECG_Bacchus_TaskRest\' list_folders{listNum} '\unitInfo' selection_lists{listNum} '.mat'];
+keys.loc(cc).folder_suffix        = selection_lists{listNum};
 
-% Bacchus left pulvinar - sagittal
-cc=cc+1;
-keys.loc(cc).tt.choice = NaN;
-keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
-keys.loc(cc).vmr_path = 'Y:\MRI\Bacchus\20191115_anatomy\dicom\0104\BA_20191115_chamber_normal_L_neurological_0.25mm.vmr';
-keys.loc(cc).z_offset_mm = 19.25; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
-keys.loc(cc).monkey_prefix = 'BA_';
-keys.loc(cc).monkey='Bac';
-keys.loc(cc).target_area='dPul_L';
-keys.loc(cc).area_color=[0.05 0.65 0.7];
-keys.loc(cc).grid_id = 'GRID.22.2';
-keys.loc(cc).significance_to_plot = 'ungrouped';
-keys.loc(cc).saggital_or_coronal = 'sagittal';
-keys.loc(cc).unit_list = 'Y:\Projects\Pulv_bodysignal\ECG_triggered_spikes\ECG_Bacchus_TaskRest\unit_lists_ECG\unitInfo_after_SNR_exclusion_stable_noLow_amplitude_ccs_any.mat';
-
-% Bacchus left MD
-cc=cc+1;
-keys.loc(cc).tt.choice = NaN;
-keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
-keys.loc(cc).vmr_path = 'Y:\MRI\Bacchus\20191115_anatomy\dicom\0104\BA_20191115_chamber_normal_L_neurological_0.25mm.vmr';
-keys.loc(cc).z_offset_mm = 19.25; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
-keys.loc(cc).monkey_prefix = 'BA_';
-keys.loc(cc).monkey='Bac';
-keys.loc(cc).target_area='MD_L';
-keys.loc(cc).area_color=[1 0 0.6];
-keys.loc(cc).grid_id = 'GRID.22.2';
-keys.loc(cc).significance_to_plot = 'ungrouped';
-keys.loc(cc).saggital_or_coronal = 'coronal';
-keys.loc(cc).unit_list = 'Y:\Projects\Pulv_bodysignal\ECG_triggered_spikes\ECG_Bacchus_TaskRest\unit_lists_ECG\unitInfo_after_SNR_exclusion_stable_noLow_amplitude_ccs_any.mat';
-
-% Bacchus left MD - sagittal
-cc=cc+1;
-keys.loc(cc).tt.choice = NaN;
-keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
-keys.loc(cc).vmr_path = 'Y:\MRI\Bacchus\20191115_anatomy\dicom\0104\BA_20191115_chamber_normal_L_neurological_0.25mm.vmr';
-keys.loc(cc).z_offset_mm = 19.25; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
-keys.loc(cc).monkey_prefix = 'BA_';
-keys.loc(cc).monkey='Bac';
-keys.loc(cc).target_area='MD_L';
-keys.loc(cc).area_color=[1 0 0.6];
-keys.loc(cc).grid_id = 'GRID.22.2';
-keys.loc(cc).significance_to_plot = 'ungrouped';
-keys.loc(cc).saggital_or_coronal = 'sagittal';
-keys.loc(cc).unit_list = 'Y:\Projects\Pulv_bodysignal\ECG_triggered_spikes\ECG_Bacchus_TaskRest\unit_lists_ECG\unitInfo_after_SNR_exclusion_stable_noLow_amplitude_ccs_any.mat';
-
-% Bacchus right MD - coronal
-cc=cc+1;
-keys.loc(cc).tt.choice = NaN;
-keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
-keys.loc(cc).vmr_path = 'Y:\MRI\Bacchus\20191115_anatomy\dicom\0103\BA_20191115_chamber_normal_R_neurological_0.25mm.vmr';
-keys.loc(cc).z_offset_mm = 23.25; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
-keys.loc(cc).monkey_prefix = 'BA_';
-keys.loc(cc).monkey='Bac';
-keys.loc(cc).target_area='MD_R';
-keys.loc(cc).area_color=[1 0 0.6];
-keys.loc(cc).grid_id = 'GRID.22.2';
-keys.loc(cc).significance_to_plot = 'ungrouped';
-keys.loc(cc).saggital_or_coronal = 'coronal';
-keys.loc(cc).unit_list = 'Y:\Projects\Pulv_bodysignal\ECG_triggered_spikes\ECG_Bacchus_TaskRest\unit_lists_ECG\unitInfo_after_SNR_exclusion_stable_noLow_amplitude_ccs_any.mat';
-
-% Bacchus right MD - sagittal
-cc=cc+1;
-keys.loc(cc).tt.choice = NaN;
-keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
-keys.loc(cc).vmr_path = 'Y:\MRI\Bacchus\20191115_anatomy\dicom\0103\BA_20191115_chamber_normal_R_neurological_0.25mm.vmr';
-keys.loc(cc).z_offset_mm = 23.25; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
-keys.loc(cc).monkey_prefix = 'BA_';
-keys.loc(cc).monkey='Bac';
-keys.loc(cc).target_area='MD_R';
-keys.loc(cc).area_color=[1 0 0.6];
-keys.loc(cc).grid_id = 'GRID.22.2';
-keys.loc(cc).significance_to_plot = 'ungrouped';
-keys.loc(cc).saggital_or_coronal = 'sagittal';
-keys.loc(cc).unit_list = 'Y:\Projects\Pulv_bodysignal\ECG_triggered_spikes\ECG_Bacchus_TaskRest\unit_lists_ECG\unitInfo_after_SNR_exclusion_stable_noLow_amplitude_ccs_any.mat';
+% % Bacchus left MD
+% cc=cc+1;
+% keys.loc(cc).tt.choice = NaN;
+% keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
+% keys.loc(cc).vmr_path = 'Y:\MRI\Bacchus\20191115_anatomy\dicom\0104\BA_20191115_chamber_normal_L_neurological_0.25mm.vmr';
+% keys.loc(cc).z_offset_mm = 19.25; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
+% keys.loc(cc).monkey_prefix = 'BA_';
+% keys.loc(cc).monkey='Bac';
+% keys.loc(cc).target_area='MD_L';
+% keys.loc(cc).area_color=[1 0 0.6];
+% keys.loc(cc).grid_id = 'GRID.22.2';
+% keys.loc(cc).significance_to_plot = 'ungrouped';
+% keys.loc(cc).saggital_or_coronal = slicePlane{1};
+% keys.loc(cc).unit_list = ['Y:\Projects\Pulv_bodysignal\ECG_triggered_spikes\ECG_Bacchus_TaskRest\' list_folders{listNum} '\unitInfo' selection_lists{listNum} '.mat'];
+% keys.loc(cc).folder_suffix        = selection_lists{listNum};
+% 
+% % Bacchus right MD
+% cc=cc+1;
+% keys.loc(cc).tt.choice = NaN;
+% keys.loc(cc).tt.tasktypes = {'Fsac_opt','Vsac_opt'};
+% keys.loc(cc).vmr_path = 'Y:\MRI\Bacchus\20191115_anatomy\dicom\0103\BA_20191115_chamber_normal_R_neurological_0.25mm.vmr';
+% keys.loc(cc).z_offset_mm = 23.25; % mm, distance from chamber top to "brain entry", should be different from 0 if z is from top of the chamber
+% keys.loc(cc).monkey_prefix = 'BA_';
+% keys.loc(cc).monkey='Bac';
+% keys.loc(cc).target_area='MD_R';
+% keys.loc(cc).area_color=[1 0 0.6];
+% keys.loc(cc).grid_id = 'GRID.22.2';
+% keys.loc(cc).significance_to_plot = 'ungrouped';
+% keys.loc(cc).saggital_or_coronal = slicePlane{1};
+% keys.loc(cc).unit_list = ['Y:\Projects\Pulv_bodysignal\ECG_triggered_spikes\ECG_Bacchus_TaskRest\' list_folders{listNum} '\unitInfo' selection_lists{listNum} '.mat'];
+% keys.loc(cc).folder_suffix        = selection_lists{listNum};
+    end
+end
